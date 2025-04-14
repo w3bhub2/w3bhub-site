97d5cdc1338202de7655c58b44ecba21db3c8f4b
w3bhub2 <w3bhub2@gmail.com>
2025-04-14 07:09:51 +0000

    Initial website setup

---
+++ components/Footer.js
@@ -0,0 +1,6 @@
+// Footer component
+const footer = `
+    <p>&copy; 2025 W3BHub. All rights reserved.</p>
+`;
+
+document.getElementById('footer').innerHTML = footer;
---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+// Navbar component
+const navbar = `
+    <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#services">Services</a>
+        <a href="#contact">Contact</a>
+    </nav>
+`;
+
+document.getElementById('navbar').innerHTML = navbar;
